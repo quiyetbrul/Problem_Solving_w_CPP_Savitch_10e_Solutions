155e1ad81480506ce261036fec148b04d8294dd4
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-05-25 09:37:42 -0700

    Ch_3 updated

--- Chapter_3.h
+++ Chapter_3.h
@@ -7,6 +7,9 @@
 using namespace std;
 
 void Ch3_Practice_Program_1();
+void Ch3_Practice_Program_2();
+void Ch3_Practice_Program_3();
+
 
 int Chapter_3() {
 	do {
@@ -15,6 +18,8 @@ int Chapter_3() {
 		cout << "Chapter 3 Solutions" << endl;
 		cout << "==============================" << endl;
 		cout << "1. Practice_Program_1" << endl;
+		cout << "2. Practice_Program_2" << endl;
+		cout << "3. Practice_Program_3" << endl;
 		cout << "0. Exit" << endl;
 		cout << "==============================" << endl;
 		cout << "Option : ";
@@ -25,7 +30,8 @@ int Chapter_3() {
 
 		switch (atoi(option)) {
 		case 1: Ch3_Practice_Program_1(); break;
-		
+		case 2: Ch3_Practice_Program_2(); break;
+		case 3: Ch3_Practice_Program_3(); break;
 		case 0: exit(0);
 		default: break;
 		}
@@ -63,5 +69,30 @@ void Ch3_Practice_Program_1() {
 
 		ans = inputChar("Repeat? ", 'y', 'n');
 	} while (ans == 'y');
+}
+
+void Ch3_Practice_Program_2() {
+	char ans = ' ';
+	do {
+		double account_balance = inputDouble("Enter account balance: ", true);
+		double interest = 0.0;
+
+		account_balance <= 1000 ? interest = 1.5 : interest = 1.0;
+
+		double total_amount_due = account_balance + (account_balance * (interest / 100));
+
+		double minimum_payment = 0.0;
+
+		total_amount_due <= 10 ? minimum_payment = total_amount_due : minimum_payment = total_amount_due * 10 / 100;
+
+		cout << "Total amount due: " << total_amount_due << endl;
+		cout << "Minimum payment: " << minimum_payment << endl;
+
+		ans = inputChar("Repeat? ", 'y', 'n');
+		cout << endl;
+	} while (ans == 'y');
+}
+
+void Ch3_Practice_Program_3() {
 
 }
