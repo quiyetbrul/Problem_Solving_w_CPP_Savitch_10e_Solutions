4fbed9b3cab74a3708af5f94f5bcf0aa11535671
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-05-30 19:49:57 -0700

    Ch3 pracprog4 finished

--- Chapter_3.h
+++ Chapter_3.h
@@ -218,14 +218,32 @@ void Ch3_Practice_Program_3()
 	cout << "Zodiac sign: " << astro_sign << endl;
 }
 
+const vector<string> FIRE {"Aries", "Leo", "Sagittarius"};
+const vector<string> EARTH {"Taurus", "Virgo", "Capricorn"};
+const vector<string> AIR {"Gemini", "Libra", "Aquarius"};
+const vector<string> WATER {"Cancer", "Scorpio", "Pisces"};
+
+void zodiac_element(vector<string> vect, int size, string astro){
+  for(int i = 0; i < size; i++){
+		if(astro == vect[i]) {
+      for(int i = 0; i < size; i++){
+        cout << vect[i] << " ";
+      }
+    }
+	}
+}
+
 void Ch3_Practice_Program_4()
 {
-	const vector<string> FIRE {"Aries", "Leo", "Sagittarius"};
-	const vector<string> EARTH {"Taurus", "Virgo", "Capricorn"};
-	const vector<string> AIR {"Gemini", "Libra", "Aquarius"};
-	const vector<string> WATER {"Cancer", "Scorpio", "Pisces"};
+  astro_sign = inputString("Enter astro sign: ", astro_sign);
 
-	cout << "You are compatible with the other ";
+  cout << "ASTRO SIGN: " << astro_sign << endl;
 
+  cout << "You are compatible with the other ";
+  zodiac_element(FIRE, FIRE.size(), astro_sign);
+  zodiac_element(AIR, AIR.size(), astro_sign);
+  zodiac_element(EARTH, EARTH.size(), astro_sign);
+  zodiac_element(WATER, WATER.size(), astro_sign);
 
+  cout << endl;
 }
