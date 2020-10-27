8afaec59c9620a2b57a62537ed90a1bfd36378df
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-10-26 23:05:04 -0700

    Update Ch3_Programming_Project_9.h

--- src/Ch3/Ch3_Programming_Project_9.h
+++ src/Ch3/Ch3_Programming_Project_9.h
@@ -2,7 +2,57 @@
 
 using namespace std;
 
+int randint(int n)
+{
+  return (1.0*n*rand())/(1.0+RAND_MAX);
+}
+ 
+int other(int doorA, int doorB)
+{
+  int doorC;
+  if (doorA == doorB)
+  {
+    doorC = randint(2);
+    if (doorC >= doorA)
+      ++doorC;
+  }
+  else
+  {
+    for (doorC = 0; doorC == doorA || doorC == doorB; ++doorC)
+    {
+      // empty
+    }
+  }
+  return doorC;
+}
+ 
+int check(int games, bool change)
+{
+  int win_count = 0;
+  for (int game = 0; game < games; ++game)
+  {
+    int const winning_door = randint(3);
+    int const original_choice = randint(3);
+    int open_door = other(original_choice, winning_door);
+ 
+    int const selected_door = change?
+                                other(open_door, original_choice)
+                              : original_choice;
+ 
+    if (selected_door == winning_door)
+      ++win_count;
+  }
+ 
+  return win_count;
+}
+
 void Ch3_Programming_Project_9(){
 
+    srand(time(0));
     
+    int games = 10000;
+    int wins_stay = check(games, false);
+    int wins_change = check(games, true);
+    cout << "staying: " << 100.0*wins_stay/games << "%, changing: " << 100.0*wins_change/games << "%\n";
+    cout << "staying: " << wins_stay << ", changing: " << wins_change << endl;
 }
