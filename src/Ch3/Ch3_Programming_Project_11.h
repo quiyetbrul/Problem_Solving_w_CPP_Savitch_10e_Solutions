63ca3aa518774c3b33a1d96e5b159fd8ee9be905
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-10-27 22:09:36 -0700

    Update Ch3_Programming_Project_11.h

--- src/Ch3/Ch3_Programming_Project_11.h
+++ src/Ch3/Ch3_Programming_Project_11.h
@@ -43,10 +43,10 @@ int find_maximum(int temp){
 bool contains_147(int temp){
     int temp_arr[3];
 
-    temp_arr[0] = (temp/100) %10; // ones
-    temp_arr[1] = (temp/10) %10; // tens
-    temp_arr[2] = temp % 10; // hundreds
-    
+    temp_arr[0] = (temp / 100) % 10; // ones
+    temp_arr[1] = (temp / 10) % 10;  // tens
+    temp_arr[2] = temp % 10;         // hundreds
+
     return (temp_arr[2] == 1 || temp_arr[2] == 4 || temp_arr[2] == 7 ||
             temp_arr[1] == 1 || temp_arr[1] == 4 || temp_arr[1] == 7 ||
             temp_arr[0] == 1 || temp_arr[0] == 4 || temp_arr[0] == 7);
