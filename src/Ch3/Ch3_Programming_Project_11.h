d7b4467722475a035a4e7c4453b05eeb57671d8e
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-10-27 22:06:53 -0700

    Create Ch3_Programming_Project_11.h

---
+++ src/Ch3/Ch3_Programming_Project_11.h
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+bool contains_147(int temp);
+int find_minimum(int temp);
+int find_maximum(int temp);
+
+void Ch3_Programming_Project_11(){
+    int temperature = inputInteger("Enter temperature: ", 0, 999);
+
+    cout << "Initial input: " << temperature << endl;
+    int temp_arr[3];
+    temp_arr[0] = (temperature/100) %10;
+    temp_arr[1] = (temperature/10) %10;
+    temp_arr[2] = temperature % 10;
+
+    cout << "Printing each number: ";
+    for(auto i : temp_arr) cout << i << " ";
+
+    cout << endl;
+
+    if(!contains_147(temperature)) cout << temperature;
+    else{
+        cout << "Minimum: " << find_minimum(temperature) << endl 
+             << "Maximum: " << find_maximum(temperature) << endl;
+    }
+
+}
+
+int find_minimum(int temp){
+    while (contains_147(temp)){
+        --temp;
+    }
+    return temp;
+}
+
+int find_maximum(int temp){
+    while (contains_147(temp)){
+        ++temp;
+    }
+    return temp;
+}
+
+bool contains_147(int temp){
+    int temp_arr[3];
+
+    temp_arr[0] = (temp/100) %10; // ones
+    temp_arr[1] = (temp/10) %10; // tens
+    temp_arr[2] = temp % 10; // hundreds
+    
+    return (temp_arr[2] == 1 || temp_arr[2] == 4 || temp_arr[2] == 7 ||
+            temp_arr[1] == 1 || temp_arr[1] == 4 || temp_arr[1] == 7 ||
+            temp_arr[0] == 1 || temp_arr[0] == 4 || temp_arr[0] == 7);
+}
