4b78cc7dbc00fdcbfcbebc156301294da1324226
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-10-30 16:48:25 -0700

    Update Ch3_Programming_Project_12.h
    if-else statement might be wonky.
    a different way could be implemented instead of using a while-loop inside else-if.

--- src/Ch3/Ch3_Programming_Project_12.h
+++ src/Ch3/Ch3_Programming_Project_12.h
@@ -6,14 +6,30 @@ void Ch3_Programming_Project_12(){
 
     int toothpicks = 23;
 
+    cout << "Game of \"23\"" << endl;
+    cout << "NUMBER OF TOOTHPICKS LEFT: " << toothpicks << endl  << endl;
+
     do{
-        int user = inputInteger("User picks up toothpicks: #", 1, 3);
+        int user = inputInteger("User picks up toothpicks: ", 1, 3);
         toothpicks -= user;
 
-        // get random number between 1-3;
-        // int comp = inputInteger("Computer picks up toothpicks: #");
-        
-    }while(toothpicks > 4)
-    
-
+        if (toothpicks > 4){
+            int comp = 4 - user;
+            cout << "Computer picks up toothpicks: " << comp << endl;
+            toothpicks -= comp;
+            cout << "NUMBER OF TOOTHPICKS LEFT: " << toothpicks << endl  << endl;
+        }else if(toothpicks >= 2 && toothpicks <= 4){
+            int comp = 0;
+            while(toothpicks > 1) {
+                toothpicks--; comp++;
+            }
+            cout << "Computer picks up toothpicks: " << comp << endl;
+            cout << "NUMBER OF TOOTHPICKS LEFT: " << toothpicks << endl  << endl;
+            cout << "Computer wins" << endl; break;
+        }else if (toothpicks == 1){
+            cout << "Computer picks up the last toothpick" << endl;
+            cout << "User wins" << endl;
+            toothpicks = 0;
+        }
+    }while(toothpicks > 0);
 }
