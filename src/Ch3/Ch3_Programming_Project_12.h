e41796e233bf88d7f9935351e3216ec6638b9931
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-10-29 18:00:31 -0700

    Update Ch3_Programming_Project_12.h

--- src/Ch3/Ch3_Programming_Project_12.h
+++ src/Ch3/Ch3_Programming_Project_12.h
@@ -6,6 +6,14 @@ void Ch3_Programming_Project_12(){
 
     int toothpicks = 23;
 
+    do{
+        int user = inputInteger("User picks up toothpicks: #", 1, 3);
+        toothpicks -= user;
+
+        // get random number between 1-3;
+        // int comp = inputInteger("Computer picks up toothpicks: #");
+        
+    }while(toothpicks > 4)
     
 
 }
