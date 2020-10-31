99724d8375edd6d74196cdd347254af26f86ab94
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-10-30 23:23:10 -0700

    Update Ch3_Programming_Project_13.h
    function different_number(int) returns true for non-adjacent duplicate numbers. WRONG WRONG WRONG

--- src/Ch3/Ch3_Programming_Project_13.h
+++ src/Ch3/Ch3_Programming_Project_13.h
@@ -15,47 +15,63 @@ void Ch3_Programming_Project_13(){
 
     int address_number = inputInteger("Guess an address number: ", 1000,9999);
 
-    while (!different_numbers(address_number) || !three_times(address_number) || !odd_number(address_number) || sum_number(address_number) != 27){
-        cout << "Sum of the guess: " << sum_number(address_number) << endl << endl;
-
-        // just to check
-        if (!different_numbers(address_number)) 
-            cout << "Each digit must be unique" << endl;
-        if (!three_times(address_number))
-            cout << address_number << " != tens*3==thousands" << endl;
-        if(!odd_number(address_number)) 
-            cout << address_number << " != odd number" << endl;
-        if (sum_number(address_number) != 27) 
-            cout << "Sum of the guess must be 27" << endl << endl;
-
-        cout << "Incorrect guess." << endl << endl;
+    while(!different_numbers(address_number)){
+        cout << "ERROR" << endl;
         address_number = inputInteger("Guess an address number: ", 1000,9999);
     }
-    
+
+    // while (!different_numbers(address_number) || !three_times(address_number) || !odd_number(address_number) || sum_number(address_number) != 27){
+    //     cout << "Sum of the guess: " << sum_number(address_number) << endl << endl;
+    //
+    //     cout << "INCORRECT GUESS." << endl;
+    //
+    //     // just to check
+    //     if (!different_numbers(address_number))
+    //         cout << "Each digit must be unique" << endl;
+    //     if (!three_times(address_number))
+    //         cout << address_number << " != tens*3==thousands" << endl;
+    //     if(!odd_number(address_number))
+    //         cout << address_number << " != odd number" << endl;
+    //     if (sum_number(address_number) != 27)
+    //         cout << "Sum of the guess must be 27" << endl;
+    //
+    //     cout << endl;
+    //
+    //     address_number = inputInteger("Guess an address number: ", 1000,9999);
+    // }
+
     cout << "Sum of the guess: " << sum_number(address_number) << endl;
     cout << "The correct address number: " << address_number << endl;
 }
 
 // this could be used for different_numbers, three_times, and sum_number
+// how do i use a function that returns a vector?
 vector<int> separate_number(int number){
-    vector<int> temp_arr;
-    temp_arr[0] = number % 10;         // ones
-    temp_arr[1] = (number / 10) % 10;  // tens
-    temp_arr[2] = (number / 100) % 10; // hundreds
-    temp_arr[3] = number / 1000;       // thousands
+    vector<int> temp_arr = {
+        (number % 10),       // ones
+        (number / 10) % 10,  // tens
+        (number / 100) % 10, // hundreds
+        (number / 1000)      // thousands
+    };
 
     return temp_arr;
 }
 
-// different_numbers is a bit wonky.
+// different_numbers(int number) is a bit wonky.
+// should return false if there're adjacent AND non-adjacent duplicate numbers,
+    // e.g. 1123, 1223, 1233, 1231, 1213, etc.
+// currently returns true for non-adjacent duplicate numbers,
+    // e.g. 1231
 bool different_numbers(int number){
-    int temp_arr[4];
-    temp_arr[0] = number % 10;         // ones
-    temp_arr[1] = (number / 10) % 10;  // tens
-    temp_arr[2] = (number / 100) % 10; // hundreds
-    temp_arr[3] = number / 1000;       // thousands
-
-    return temp_arr[0] != temp_arr[1] != temp_arr[2] != temp_arr[3];
+    vector<int> temp_arr = {
+        (number % 10),       // ones
+        (number / 10) % 10,  // tens
+        (number / 100) % 10, // hundreds
+        (number / 1000)      // thousands
+    };
+    
+    auto dif = unique(temp_arr.begin(), temp_arr.end());
+    return dif==temp_arr.end();
 }
 
 bool three_times(int number){
@@ -68,7 +84,7 @@ bool three_times(int number){
 }
 
 bool odd_number(int number){
-    return number%2==0;
+    return number%2!=0;
 }
 
 int sum_number(int number) {
