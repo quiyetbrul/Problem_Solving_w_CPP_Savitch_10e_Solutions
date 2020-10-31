480852b9c78275d8a6a9c74b8221dc9942bf0c33
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-10-31 11:37:32 -0700

    Create Ch3_Programming_Project_14.h

---
+++ src/Ch3/Ch3_Programming_Project_14.h
@@ -0,0 +1,18 @@
+void Ch3_Programming_Project_14(){
+
+}
+
+// ask how many edoc they have
+// ask how many candies they have
+
+// loop - start
+// ask how many edoc will be transferred
+    // edoc or margorp
+        // addition of candies
+        // calc of how many edoc/margorp remains
+        
+// ask how many edoc to evolve to margorp
+        // calc of exp
+
+// cout confirmation
+// loop - end
