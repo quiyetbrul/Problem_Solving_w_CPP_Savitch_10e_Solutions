d954f0b3a17ab9c8b167511f2511ef9800cb59d8
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-10-30 19:36:50 -0700

    Update Ch3_Programming_Project_13.h
    different_numbers(int number) is a wonky function.
    
    i want to use vector for the functions different_numbers, three_times, and sum_number.

--- src/Ch3/Ch3_Programming_Project_13.h
+++ src/Ch3/Ch3_Programming_Project_13.h
@@ -2,6 +2,81 @@
 
 using namespace std;
 
+// this could be used for different_numbers, three_times, and sum_number
+vector<int> separate_number(int number);
+
+bool different_numbers(int number);
+bool three_times(int number);
+bool odd_number(int number);
+int sum_number(int number);
+
 void Ch3_Programming_Project_13(){
+    // correct answer is 9738 || 9837
+
+    int address_number = inputInteger("Guess an address number: ", 1000,9999);
+
+    while (!different_numbers(address_number) || !three_times(address_number) || !odd_number(address_number) || sum_number(address_number) != 27){
+        cout << "Sum of the guess: " << sum_number(address_number) << endl << endl;
+
+        // just to check
+        if (!different_numbers(address_number)) 
+            cout << "Each digit must be unique" << endl;
+        if (!three_times(address_number))
+            cout << address_number << " != tens*3==thousands" << endl;
+        if(!odd_number(address_number)) 
+            cout << address_number << " != odd number" << endl;
+        if (sum_number(address_number) != 27) 
+            cout << "Sum of the guess must be 27" << endl << endl;
+
+        cout << "Incorrect guess." << endl << endl;
+        address_number = inputInteger("Guess an address number: ", 1000,9999);
+    }
     
+    cout << "Sum of the guess: " << sum_number(address_number) << endl;
+    cout << "The correct address number: " << address_number << endl;
+}
+
+// this could be used for different_numbers, three_times, and sum_number
+vector<int> separate_number(int number){
+    vector<int> temp_arr;
+    temp_arr[0] = (number / 100) % 10; // ones
+    temp_arr[1] = (number / 10) % 10;  // tens
+    temp_arr[2] = number % 10;         // hundreds
+    temp_arr[3] = number / 1000;       // thousands
+
+    return temp_arr;
+}
+
+// different_numbers is a bit wonky.
+bool different_numbers(int number){
+    int temp_arr[4];
+    temp_arr[0] = (number / 100) % 10; // ones
+    temp_arr[1] = (number / 10) % 10;  // tens
+    temp_arr[2] = number % 10;         // hundreds
+    temp_arr[3] = number / 1000;       // thousands
+
+    return temp_arr[0] != temp_arr[1] != temp_arr[2] != temp_arr[3];
+}
+
+bool three_times(int number){
+    // find thousands place
+    int thousands = number / 1000;
+    // find tens place
+    int tens = (number / 10) % 10;
+
+    return tens*3==thousands;
+}
+
+bool odd_number(int number){
+    return number%2==0;
+}
+
+int sum_number(int number) {
+	int sum_number = 0;
+	while (number > 0) {
+		sum_number += number % 10;
+		number /= 10;
+	}
+    
+	return sum_number;
 }
