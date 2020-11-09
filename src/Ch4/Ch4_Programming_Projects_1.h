961d4cb572ccae7caa75d4f4d3996a90dba02fdf
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-11-08 22:57:21 -0800

    Create Ch4_Programming_Projects_1.h

---
+++ src/Ch4/Ch4_Programming_Projects_1.h
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+double annual_mortgage_cost(double initial_loan_balance){
+    return initial_loan_balance .09;
+}
+
+double tax_savings(double initial_loan_balance){
+    return initial_loan_balance * .021;
+}
+
+double initial_loan_balance(double house_price, double down_payment){
+    return house_price-down_payment;
+}
+
+void Ch4_Programming_Projects_1(){
+
+    
+}
+
