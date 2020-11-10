5cbc81274f76b43d1c5e8605f5759f6414002e44
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-11-09 17:42:01 -0800

    Update Ch4_Programming_Project_1.h

--- src/Ch4/Ch4_Programming_Project_1.h
+++ src/Ch4/Ch4_Programming_Project_1.h
@@ -2,20 +2,32 @@
 
 using namespace std;
 
-double annual_mortgage_cost(double initial_loan_balance){
+double find_annual_mortgage_cost(double initial_loan_balance){
     return initial_loan_balance * .09;
 }
 
-double tax_savings(double initial_loan_balance){
+double find_tax_savings(double initial_loan_balance){
     return initial_loan_balance * .021;
 }
 
-double initial_loan_balance(double house_price, double down_payment){
+double find_initial_loan_balance(double house_price, double down_payment){
     return house_price-down_payment;
 }
 
 void Ch4_Programming_Project_1(){
 
-    
-}
+    double initial_balance = 0, annual_morgage_cost = 0, tax_savings = 0;;
+    double house_price = inputDouble("Enter house cost: ", true);
+    double down_payment = inputDouble("Enter down payment: ", true);
 
+    initial_balance = find_initial_loan_balance(house_price, down_payment);
+    annual_morgage_cost = find_annual_mortgage_cost(initial_balance);
+    tax_savings = find_tax_savings(initial_balance);
+
+    cout  << "Initial balance: $" << initial_balance << endl;
+    cout << "Annual after-tax cost of home ownership: $" << abs(annual_morgage_cost - tax_savings);
+    cout << "Initial loan balance: $" << initial_balance << endl;
+    cout << "Annual mortgage: $" << annual_morgage_cost << endl;
+    cout << "Tax savings: $" << tax_savings << endl;
+
+}
