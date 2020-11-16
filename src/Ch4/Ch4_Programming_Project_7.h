fc09791fb8ff9ee445f28886306d01f9b942969e
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-11-15 20:12:26 -0800

    Update Ch4_Programming_Project_7.h

--- src/Ch4/Ch4_Programming_Project_7.h
+++ src/Ch4/Ch4_Programming_Project_7.h
@@ -1,12 +1,15 @@
- #include <iostream>
+#include <iostream>
 
- using namespace std;
+using namespace std;
 
 double compute_difference(int hour, int minute, bool is_AM){
 
-    return;
+    return 0;
 }
 
 void Ch4_Programming_Project_7(){
-
+    int starting_hour = inputInteger("Starting hour: ", 1,12);
+    int starting_minute = inputInteger("Starting minute: ", 1,60);
+    bool is_AM;
+    
 }
