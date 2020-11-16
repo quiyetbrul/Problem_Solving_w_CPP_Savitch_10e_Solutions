fac520bd0afc78781e025c8049bada1b0e47a2d6
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-11-15 20:26:02 -0800

    Update Ch4_Programming_Project_7.h

--- src/Ch4/Ch4_Programming_Project_7.h
+++ src/Ch4/Ch4_Programming_Project_7.h
@@ -2,14 +2,22 @@
 
 using namespace std;
 
-double compute_difference(int hour, int minute, bool is_AM){
+double compute_difference(int starting_hour, int starting_minute, int ending_hour, int ending_minute, bool starting_meridiem, bool ending_meridiem){
 
     return 0;
 }
 
+
+/*
+    -one way of solving this problem is to first figure out if it's AM/PM
+        and then change the time into military time and multiply everything by 60
+        to then get the difference
+            --potential problem is getting difference between days
+*/
+
 void Ch4_Programming_Project_7(){
     int starting_hour = inputInteger("Starting hour: ", 1,12);
     int starting_minute = inputInteger("Starting minute: ", 1,60);
-    bool is_AM;
+    bool starting_meridiem, ending_meridiem;
     
 }
