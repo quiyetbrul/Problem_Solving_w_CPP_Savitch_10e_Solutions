d3e654f60eda72afa08de228ea296644d18c9707
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-11-16 19:02:37 -0800

    Update Ch4_Programming_Project_7.h
    noon-pm, pm-midnight, am-pm, pm-am are wonky

--- src/Ch4/Ch4_Programming_Project_7.h
+++ src/Ch4/Ch4_Programming_Project_7.h
@@ -11,19 +11,34 @@ int compute_difference(int starting_hour, int starting_minute, bool starting_mer
     if (starting_hour > 12 && !starting_meridiem) starting_hour += 12;
     if (ending_hour > 12 && !ending_meridiem) ending_hour += 12;
 
-    if ((starting_meridiem && ending_meridiem) || (!starting_meridiem && !ending_meridiem)) // condition if input time are from the same day or not
+    // if (starting_hour < ending_hour)                    // ex. (10:00 AM && 11:00 AM)|| (1:00 PM && 2:00 PM) // same day
+    //     time_difference = (abs)(starting_hour - ending_hour) * 60;
+    // else if (starting_hour > ending_hour)               // ex. (11:00 AM && 10:00 AM)|| (2:00 PM && 1:00 PM) // following day
+    //     time_difference = (abs)(starting_hour - ending_hour - 24) * 60;
+    // else if (starting_hour == ending_hour)              // ex. (11:00 AM && 11:00 AM)|| (1:00 PM && 1:00 PM) // 24-hour difference
+    //     time_difference = 24 * 60;
+
+    // calculate hours
+    if ((starting_meridiem && ending_meridiem) || (!starting_meridiem && !ending_meridiem)) // same_day? same_meridiem?
     {
-        if (starting_hour < ending_hour) // ex. (10:00 AM && 11:00 AM)|| (1:00 PM && 2:00 PM)
+        if (starting_hour < ending_hour)                    // ex. (10:00 AM && 11:00 AM)|| (1:00 PM && 2:00 PM) // same day
             time_difference = (abs)(starting_hour - ending_hour) * 60;
-        else if (starting_hour > ending_hour) // ex. (11:00 AM && 10:00 AM)|| (2:00 PM && 1:00 PM)
+        else if (starting_hour > ending_hour)               // ex. (11:00 AM && 10:00 AM)|| (2:00 PM && 1:00 PM) // following day
             time_difference = (abs)(starting_hour - ending_hour - 24) * 60;
-        else time_difference = 24 * 60; // ex. (11:00 AM && 11:00 AM)|| (1:00 PM && 1:00 PM) // for the following day
+        else if (starting_hour == ending_hour)              // ex. (11:00 AM && 11:00 AM)|| (1:00 PM && 1:00 PM) // 24-hour difference
+            time_difference = 24 * 60;
     }
-    else // calc AM to PM and PM to AM are wonky
+    else if (starting_meridiem && !ending_meridiem) // AM to PM
+    {
         time_difference = (abs)(starting_hour - ending_hour) * 60;
-    
+    }
+    else if (!starting_meridiem && ending_meridiem) // PM to AM
+    {
+        time_difference = (abs)(starting_hour - ending_hour) * 60;
+    }
 
 
+    // calculate minutes
     if (starting_minute < ending_minute)
         time_difference += (abs)(starting_minute - ending_minute);
     else
@@ -42,13 +57,34 @@ int compute_difference(int starting_hour, int starting_minute, bool starting_mer
 */
 
 void Ch4_Programming_Project_7(){
-    int starting_hour = inputInteger("Starting hour: ", 1,12);
-    int starting_minute = inputInteger("Starting minute: ", 0,60);
-    bool starting_meridiem = inputInteger("Starting meridiem (1-am, 0-pm): ", 0,1);
-    int ending_hour = inputInteger("Ending hour: ", 1,12);
-    int ending_minute = inputInteger("Ending minute: ", 0,60);
-    bool ending_meridiem = inputInteger("Ending meridiem (1-am, 0-pm): ", 0,1);
+    // int starting_hour = inputInteger("Starting hour: ", 1,12);
+    // int starting_minute = inputInteger("Starting minute: ", 0,60);
+    // bool starting_meridiem = inputInteger("Starting meridiem (1-am, 0-pm): ", 0,1);
 
-    cout << compute_difference(starting_hour, starting_minute, starting_meridiem, ending_hour, ending_minute, ending_meridiem) 
-         << " minute(s) difference" << endl;
+    // int ending_hour = inputInteger("Ending hour: ", 1,12);
+    // int ending_minute = inputInteger("Ending minute: ", 0,60);
+    // bool ending_meridiem = inputInteger("Ending meridiem (1-am, 0-pm): ", 0,1);
+
+    // cout << compute_difference(starting_hour, starting_minute, starting_meridiem, ending_hour, ending_minute, ending_meridiem) 
+    //      << " minute(s) difference" << endl;
+    
+    cout << "same day,           10AM/11AM    " << compute_difference(10, 0, 1, 11, 0, 1) << " minute(s) difference" << endl; // ex. (10:00 AM && 11:00 AM)|| (1:00 PM && 2:00 PM) // same day
+    cout << "same day,           1PM/2PM      " << compute_difference(1, 0, 0, 2, 0, 0) << " minute(s) difference" << endl << endl; // ex. (10:00 AM && 11:00 AM)|| (1:00 PM && 2:00 PM) // same day
+
+    cout << "following day,      11AM/10AM    " << compute_difference(11, 0, 1, 10, 0, 1) << " minute(s) difference" << endl; // ex. (11:00 AM && 10:00 AM)|| (2:00 PM && 1:00 PM) // following day
+    cout << "following day,      2PM/1PM      " << compute_difference(2, 0, 0, 1, 0, 0) << " minute(s) difference" << endl << endl; // ex. (11:00 AM && 10:00 AM)|| (2:00 PM && 1:00 PM) // following day
+
+    cout << "24-hour,            1AM/1AM      " << compute_difference(1, 0, 1, 1, 0, 1) << " minute(s) difference" << endl; // ex. 24-hour difference
+    cout << "24-hour,            1PM/1PM      " << compute_difference(1, 0, 0, 1, 0, 0) << " minute(s) difference" << endl << endl; // ex. 24-hour difference
+
+    cout << "AM-NOON,            11AM/12PM    " << compute_difference(11, 0, 1, 12, 0, 0) << " minute(s) difference" << endl; // ex. AM-PM difference
+    cout << "NOON-PM,            12PM/1PM     " << compute_difference(12, 0, 0, 1, 0, 0) << " minute(s) difference" << endl << endl; // ex. NOON-PM difference
+
+    cout << "PM-MIDNIGHT,        11PM/12AM    " << compute_difference(11, 0, 0, 12, 0, 1) << " minute(s) difference" << endl; // ex. PM-MIDNIGHT difference
+    cout << "MIDNIGHT-AM,        12AM/1AM     " << compute_difference(12, 0, 1, 1, 0, 1) << " minute(s) difference" << endl << endl; // ex. PM-MIDNIGHT difference
+    
+    cout << "AM-PM,              11AM/1PM     " << compute_difference(11, 0, 1, 1, 0, 0) << " minute(s) difference" << endl; // ex. NOON-PM difference
+    cout << "PM-AM,              11PM/1AM     " << compute_difference(11, 0, 0, 1, 0, 1) << " minute(s) difference" << endl << endl; // ex. PM-MIDNIGHT difference
+
+    
 }
