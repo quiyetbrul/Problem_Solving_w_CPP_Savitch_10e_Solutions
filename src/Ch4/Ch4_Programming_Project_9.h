5fd8c3b814effe471650e5842dcf0ec5d0c684a3
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-11-19 00:02:07 -0800

    Update Ch4_Programming_Project_9.h
    not a huge fan of the nested if-else statements

--- src/Ch4/Ch4_Programming_Project_9.h
+++ src/Ch4/Ch4_Programming_Project_9.h
@@ -7,51 +7,77 @@ const vector<string> TEAM{
     "Last place team",
     "Second-to-last place team",
     "Third-to-last place team",
-    "Fourth-to-last place team"};
+    "Fourth-to-last place team"
+    };
 
 int random_number(int lo, int hi);
 string draft_ball(vector<string> team, int random_number);
 
-
 void Ch4_Programming_Project_9()
 {
-    char ans = ' ';
+    int count = 0;
+    int team_count[4] = {0,0,0,0};
+    string temp = "";
     do
     {
-        int rand_num = random_number(1,40);
-        int temp = 0;
-        if(temp == rand_num) cout << "pick again" << endl;
-        else{
-            if(draft_ball(TEAM, rand_num) == TEAM[0]){
+        int rand_num = random_number(1, 40);
+        if (temp == draft_ball(TEAM, rand_num))
+        {
+            cout << "Don't be greedy, " << temp << ". Let the other teams pick.";
+            count--;
+        }
+        else
+        {
+            if (draft_ball(TEAM, rand_num) == TEAM[0])
+            {
                 cout << TEAM[0] << " picks " << rand_num;
-            }else if(draft_ball(TEAM, rand_num) == TEAM[1]){
+                temp = TEAM[0];
+                team_count[0]++;
+            }
+            else if (draft_ball(TEAM, rand_num) == TEAM[1])
+            {
                 cout << TEAM[1] << " picks " << rand_num;
-            }else if(draft_ball(TEAM, rand_num) == TEAM[2]){
+                temp = TEAM[1];
+                team_count[1]++;
+            }
+            else if (draft_ball(TEAM, rand_num) == TEAM[2])
+            {
                 cout << TEAM[2] << " picks " << rand_num;
-            }else if(draft_ball(TEAM, rand_num) == TEAM[3]){
+                temp = TEAM[2];
+                team_count[2]++;
+            }
+            else if (draft_ball(TEAM, rand_num) == TEAM[3])
+            {
                 cout << TEAM[3] << " picks " << rand_num;
+                temp = TEAM[3];
+                team_count[3]++;
             }
         }
-
         cout << endl;
+        count++;
+    } while (count != 40);
 
-        temp = rand_num;
+    cout << endl;
 
-        ans = inputChar("Repeat? ", 'y', 'n');
-    }while (ans == 'y');
+    for(int i = 0; i < TEAM.size(); i ++){
+        cout << TEAM[i] << " picked " << team_count[i] << " people." << endl;
+    }
 }
 
 // takes in minimum and maximum range of numbers
 // returns randomized number within given range
-int random_number(int lo, int hi) {
-	return rand() % (hi - lo) + lo;
+int random_number(int lo, int hi)
+{
+    return rand() % (hi - lo) + lo;
 }
 
 // takes in the team and the randomized number
 // returns which team is drafted
-string draft_ball(vector<string> team, int random_number){
-    if(random_number <=20) return team[0];
-    else if (random_number >20 || random_number <=30) return team[1];
-    else if(random_number >31 || random_number <=36) return team[2];
-    else if(random_number>36) return team[3];
+string draft_ball(vector<string> team, int random_number)
+{
+    return (random_number <= 20) ? team[0] :
+           (random_number <= 30) ? team[1] :
+           (random_number <= 36) ? team[2] :
+           (random_number > 36)  ? team[3] :
+            "ERROR Ch4_Programming_Project_9: draft_ball(vector<string>, int) function";
 }
