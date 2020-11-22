03f9b8570e1cb9c58aa1095e65c6316cf39ccdaa
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-11-22 01:51:54 -0800

    Update Ch5_Practice_Program_4.h

--- src/Ch5/Ch5_Practice_Program_4.h
+++ src/Ch5/Ch5_Practice_Program_4.h
@@ -2,40 +2,21 @@
 
 using namespace std;
 
-void get_input(double& ftm, double& incm, bool& switch_trigger){
-    switch_trigger = inputInteger("Convert 0-Imperial(ft&in to m&cm) or 1-Metric(m&cm to ftin): ", 0,1);
-    if(switch_trigger){
-        ftm = inputDouble("Enter meter: ", true);
-        incm = inputDouble("Enter centimeter: ", true);
-    }else{
-        ftm = inputDouble("Enter feet: ", true);
-        incm = inputDouble("Enter inches: ", true);
-    }
-}
+void get_output_mcm_ftin(double& ftm, double& incm);
+void get_output_ftin_mcm(double& ftm, double& incm);
 
-void mcm_ftin(double& ftm, double& incm);
-void ftin_mcm(double& ftm, double& incm);
-
-void get_output(double& ftm, double& incm, bool& switch_trigger){
-    get_input(ftm,incm,switch_trigger);
-    if(switch_trigger){
-        cout << "Before conversion: " << endl;
-        cout << ftm << "m & " << incm << " cm" << endl;
-        mcm_ftin(ftm,incm);
-        cout << "After conversion: " << endl;
-        cout << ftm << "ft & " << incm << " in" << endl;
+void get_input_ftinmcm(double& ftm, double& incm, int& switch_trigger){
+    switch_trigger = inputInteger("Convert 1-Imperial(m&cm to ft&in) or 2-Metric(ft&in to m&cm): ", 1,2);
+    if(switch_trigger == 1){
+        get_output_mcm_ftin(ftm,incm);
     }else{
-        cout << "Before conversion: " << endl;
-        cout << ftm << "ft & " << incm << " in" << endl;
-        ftin_mcm(ftm,incm);
-        cout << "After conversion: " << endl;
-        cout << ftm << "m & " << incm << " cm" << endl;
+        get_output_ftin_mcm(ftm,incm);
     }
 }
 
 void Ch5_Practice_Program_4(){
     double ftm = 0, incm = 0;
-    bool switch_trigger;
+    int switch_trigger;
 
-    get_output(ftm,incm,switch_trigger);
+    get_input_ftinmcm(ftm,incm,switch_trigger);
 }
