0199b74709b3b162c12c3157806639586d061da8
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-11-22 00:49:44 -0800

    Update Chapter_5.cpp

--- src/Ch5/Chapter_5.cpp
+++ src/Ch5/Chapter_5.cpp
@@ -5,6 +5,8 @@ using namespace std;
 #include "Ch5_Practice_Program_1.h"
 #include "Ch5_Practice_Program_2.h"
 #include "Ch5_Practice_Program_3.h"
+#include "Ch5_Practice_Program_4.h"
+// #include "Ch5_Practice_Program_5.h"
 
 int Chapter_5()
 {
@@ -17,6 +19,7 @@ int Chapter_5()
 		cout << "1. Practice_Program_1" << endl;
 		cout << "2. Practice_Program_2" << endl;
 		cout << "3. Practice_Program_3" << endl;
+		cout << "4. Practice_Program_4" << endl;
 		cout << "0. Exit" << endl;
 		cout << "==============================" << endl;
 		int option = inputInteger("Option: ", 0, 21);
@@ -24,11 +27,12 @@ int Chapter_5()
 		cout << endl;
 
 		switch (option) {
-		case 1: Ch5_Practice_Program_1(); break;
-		case 2: Ch5_Practice_Program_2(); break;
-		case 3: Ch5_Practice_Program_3(); break;
-		case 0: exit(0);
-		default: cout << "Ch5_Menu: ERROR SELECTION" << endl; break;
+			case 1: Ch5_Practice_Program_1(); break;
+			case 2: Ch5_Practice_Program_2(); break;
+			case 3: Ch5_Practice_Program_3(); break;
+			case 4: Ch5_Practice_Program_4(); break;
+			case 0: exit(0);
+			default: cout << "Ch5_Menu: ERROR SELECTION" << endl; break;
 		}
 	} while (true);
 }
