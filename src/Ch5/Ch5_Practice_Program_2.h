6d3c1ce45f95964f8a52b218b99c3d1c114e8261
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-11-22 01:51:49 -0800

    Update Ch5_Practice_Program_2.h

--- src/Ch5/Ch5_Practice_Program_2.h
+++ src/Ch5/Ch5_Practice_Program_2.h
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void get_input_imperial(double& feet, double& inches){
+void get_input_ftin_mcm(double& feet, double& inches){
     feet = inputDouble("Enter feet: ", true);
     inches = inputDouble("Enter inches: ", true);
 }
@@ -12,8 +12,8 @@ void ftin_mcm(double& feet, double& inches){
     inches *= 2.54;
 }
 
-void get_output_imperial(double& feet, double&inches){
-    get_input_imperial(feet,inches);
+void get_output_ftin_mcm(double& feet, double&inches){
+    get_input_ftin_mcm(feet,inches);
     cout << "Before conversion: " << endl;
     cout << feet << "ft & " << inches << " in" << endl;
     ftin_mcm(feet,inches);
@@ -23,5 +23,5 @@ void get_output_imperial(double& feet, double&inches){
 
 void Ch5_Practice_Program_2(){
     double feet = 0, inches = 0;
-    get_output_imperial(feet, inches);
+    get_output_ftin_mcm(feet, inches);
 }
