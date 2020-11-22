3e309f7be218c98de590e09ce71c6ff021b03481
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-11-22 01:51:38 -0800

    Update Chapter_5.cpp

--- src/Ch5/Chapter_5.cpp
+++ src/Ch5/Chapter_5.cpp
@@ -6,7 +6,10 @@ using namespace std;
 #include "Ch5_Practice_Program_2.h"
 #include "Ch5_Practice_Program_3.h"
 #include "Ch5_Practice_Program_4.h"
-// #include "Ch5_Practice_Program_5.h"
+#include "Ch5_Practice_Program_5.h"
+#include "Ch5_Practice_Program_6.h"
+#include "Ch5_Practice_Program_7.h"
+#include "Ch5_Practice_Program_8.h"
 
 int Chapter_5()
 {
@@ -20,9 +23,13 @@ int Chapter_5()
 		cout << "2. Practice_Program_2" << endl;
 		cout << "3. Practice_Program_3" << endl;
 		cout << "4. Practice_Program_4" << endl;
+		cout << "5. Practice_Program_5" << endl;
+		cout << "6. Practice_Program_6" << endl;
+		cout << "7. Practice_Program_7" << endl;
+		cout << "8. Practice_Program_8" << endl;
 		cout << "0. Exit" << endl;
 		cout << "==============================" << endl;
-		int option = inputInteger("Option: ", 0, 21);
+		int option = inputInteger("Option: ", 0, 19);
 
 		cout << endl;
 
@@ -31,6 +38,10 @@ int Chapter_5()
 			case 2: Ch5_Practice_Program_2(); break;
 			case 3: Ch5_Practice_Program_3(); break;
 			case 4: Ch5_Practice_Program_4(); break;
+			case 5: Ch5_Practice_Program_5(); break;
+			case 6: Ch5_Practice_Program_6(); break;
+			case 7: Ch5_Practice_Program_7(); break;
+			case 8: Ch5_Practice_Program_8(); break;
 			case 0: exit(0);
 			default: cout << "Ch5_Menu: ERROR SELECTION" << endl; break;
 		}
