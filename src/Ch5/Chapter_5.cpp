dcc966e405567e210ce28d73f7ac66627a6e9250
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-11-21 23:26:23 -0800

    Update Chapter_5.cpp

--- src/Ch5/Chapter_5.cpp
+++ src/Ch5/Chapter_5.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+#include "Ch5_Practice_Program_1.h"
 
 int Chapter_5()
 {
@@ -11,7 +12,7 @@ int Chapter_5()
 
 		cout << "Chapter 5 Solutions" << endl;
 		cout << "==============================" << endl;
-
+		cout << "1. Practice_Program_1" << endl;
 		cout << "0. Exit" << endl;
 		cout << "==============================" << endl;
 		int option = inputInteger("Option: ", 0, 21);
@@ -19,7 +20,7 @@ int Chapter_5()
 		cout << endl;
 
 		switch (option) {
-
+		case 1: Ch5_Practice_Program_1();
 		case 0: exit(0);
 		default: cout << "Ch5_Menu: ERROR SELECTION" << endl; break;
 		}
