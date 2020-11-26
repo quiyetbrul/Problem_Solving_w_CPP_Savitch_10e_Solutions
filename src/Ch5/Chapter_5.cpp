4fe7ccb11bccd785f89f08e56eafe314f6e782b7
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-11-25 23:24:48 -0800

    Update Chapter_5.cpp

--- src/Ch5/Chapter_5.cpp
+++ src/Ch5/Chapter_5.cpp
@@ -12,6 +12,7 @@ using namespace std;
 #include "Ch5_Practice_Program_8.h"
 #include "Ch5_Practice_Program_9.h"
 #include "Ch5_Programming_Project_1.h"
+#include "Ch5_Programming_Project_2.h"
 
 int Chapter_5(){
 	do{
@@ -29,6 +30,7 @@ int Chapter_5(){
 		cout << "8. Practice_Program_8" << endl;
 		cout << "9. Practice_Program_9" << endl;
 		cout << "10. Programming_Project_1" << endl;
+		cout << "11. Programming_Project_2" << endl;
 		cout << "0. Exit" << endl;
 		cout << "==============================" << endl;
 		int option = inputInteger("Option: ", 0, 19);
@@ -46,6 +48,7 @@ int Chapter_5(){
 			case 8: Ch5_Practice_Program_8(); break;
 			case 9: Ch5_Practice_Program_9(); break;
 			case 10: Ch5_Programming_Project_1(); break;
+			case 11: Ch5_Programming_Project_2(); break;
 			case 0: exit(0);
 			default: cout << "Ch5_Menu: ERROR SELECTION" << endl; break;
 		}
