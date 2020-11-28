da133fb21f85b905757c21275a3c2d616dbc21c6
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-11-27 18:46:09 -0800

    Update Ch5_Programming_Project_2.h
    reused a function from programming project #1
    
    -while loop still wonky

--- src/Ch5/Ch5_Programming_Project_2.h
+++ src/Ch5/Ch5_Programming_Project_2.h
@@ -2,15 +2,12 @@
 
 using namespace std;
 
-void time_input_now_later(int& hour, int& minute){
-    hour = inputInteger("Hour: ", 0,23);
-    minute = inputInteger("Minute: ", 0,59);
-}
+void time_input(int& hour, int& minute);
 
 void calc_time_waiting(int& hour_difference, int& minute_difference){
     int hour_now, minute_now, hour_later, minute_later;
-    time_input_now_later(hour_now, minute_now);
-    time_input_now_later(hour_later, minute_later);
+    time_input(hour_now, minute_now);
+    time_input(hour_later, minute_later);
 
     while(hour_now != hour_later){
         if(hour_now >= 24) hour_now = 0;
