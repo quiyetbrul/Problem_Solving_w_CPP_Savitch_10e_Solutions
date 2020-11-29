6b4b8c4e0939621037eac83e91b44a71a2064b19
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-11-28 18:37:41 -0800

    Update Ch5_Programming_Project_5.h
    minor formatting

--- src/Ch5/Ch5_Programming_Project_5.h
+++ src/Ch5/Ch5_Programming_Project_5.h
@@ -4,9 +4,10 @@ using namespace std;
 
 double windchill_index(double& wind_speed, double& temperature)
 {
-	double windchill_index = 13.12 + 0.6215 * temperature
-		- 11.37 * pow(wind_speed, 0.16)
-		+ 0.3965 * temperature * pow(wind_speed, 0.016);
+	double windchill_index = 13.12 
+						   + 0.6215 * temperature
+						   - 11.37 * pow(wind_speed, 0.16)
+						   + 0.3965 * temperature * pow(wind_speed, 0.016);
 	return windchill_index;
 }
 void Ch5_Programming_Project_5()
